6db7589c5097bf0c21a68e9460ae1f1a87210cfc
cfreeley <freeley.c@husky.neu.edu>
2014-12-17 19:55:53 -0500

    generated angular

---
+++ scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name genericDragAndDropEditorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the genericDragAndDropEditorApp
+ */
+angular.module('genericDragAndDropEditorApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name genericDragAndDropEditorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the genericDragAndDropEditorApp
+ */
+angular.module('genericDragAndDropEditorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
